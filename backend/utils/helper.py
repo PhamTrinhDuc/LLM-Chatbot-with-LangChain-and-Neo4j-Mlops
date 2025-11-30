ee0d7b381f986a95a1567eedc89100edba7c3872
PhamTrinhDuc <duc78240@gmail.com>
2025-12-01 00:47:47 +0700

    update utils

--- backend/utils/helper.py
+++ backend/utils/helper.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from typing import Literal
 from utils.config import AppConfig
 from utils.logging import logger
@@ -85,3 +86,14 @@ class ModelFactory:
       api_key=AppConfig.OPENAI_API_KEY, 
     )
     return embedding_model 
+  
+  
+def save_json(data: dict, output_path: str): 
+   with open(output_path, "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def load_json(path: str): 
+  with open(path, "r", encoding="utf-8") as f:
+    chunks = json.load(f)  # không phải json.loads
+  return chunks
